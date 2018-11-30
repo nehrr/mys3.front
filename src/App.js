23fa41d29753b380518c738b5b24e29df752aade
ERNOUL <iamkapitaan@gmail.com>
2018-11-30 09:19:24 +0100

    menu + defining views

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { TabNavigation, Tab } from "evergreen-ui";
+
+const tab = [
+  { name: "Home", url: "/" },
+  { name: "Register", url: "/register" }
+];
+
+const tabCo = [
+  { name: "Home", url: "/" },
+  { name: "Dashboard", url: "/dashboard" },
+  { name: "Profile", url: "/profile" },
+  { name: "Logout", url: "/" }
+];
 
 class App extends Component {
-  render() {
+  state = {
+    isConnected: false
+  };
+
+  _menu = array => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <Fragment>
+          <div>
+            <TabNavigation>
+              {array.map((tab, index) => (
+                <Tab key={tab.name} href={tab.url} id={tab.name}>
+                  <Link to={tab.url}> {tab.name}</Link>
+                </Tab>
+              ))}
+            </TabNavigation>
+          </div>
+        </Fragment>
+      </Router>
     );
+  };
+
+  render() {
+    const { isConnected } = this.state;
+    if (!isConnected) {
+      return this._menu(tab);
+    } else {
+      return this._menu(tabCo);
+    }
   }
 }
 

f42069c5830f19216ad3ab772da8fb0d7dcfc957
ERNOUL <iamkapitaan@gmail.com>
2018-12-01 12:06:44 +0100

    updating bucket name

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import jwt from "jsonwebtoken";
-import { Table, TextInput, Button, toaster } from "evergreen-ui";
+import { Table, TextInput, Button, toaster, Dialog } from "evergreen-ui";
 
 export default class Dashboard extends Component {
   //arbo
-  state = { bucket: "", isLoading: true };
+  state = { bucket: "", isLoading: true, isShown: false };
 
   _update(field, value) {
     this.setState({ [field]: value });
@@ -68,27 +68,116 @@ export default class Dashboard extends Component {
     this.setState({ buckets, isLoading: false });
   }
 
-  _renderTable() {
-    const { buckets } = this.state;
+  async _updateBucket(name) {
+    let uuid, token;
+    const { id } = this.state;
+    console.log(name, this.state.id);
+    const meta = JSON.parse(localStorage.getItem("myS3.app"));
+    if (meta) {
+      const decoded = jwt.decode(meta);
+      uuid = decoded.uuid;
+      token = meta;
+    }
+    const data = await fetch(
+      `http://localhost:5000/api/users/${uuid}/buckets/${id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ name })
+      }
+    );
+
+    if (data.status === 400) {
+      this._err("Uh oh, something went wrong");
+    } else {
+      toaster.notify("Your bucket was updated");
+      const buckets = await this._fetchBuckets();
+      this.setState({ buckets });
+    }
+
+    this.setState({ id: null });
+  }
+
+  _openDialog() {
+    console.log(this.state);
+    this.setState({ isShown: true });
     return (
-      <Table>
-        <Table.Head>
-          <Table.TextHeaderCell>Bucket name</Table.TextHeaderCell>
-        </Table.Head>
-        <Table.Body height={240}>
-          {buckets.map(bucket => {
-            return (
-              <Table.Row key={bucket.id}>
-                <Table.TextCell>{bucket.name}</Table.TextCell>
-              </Table.Row>
-            );
-          })}
-        </Table.Body>
-      </Table>
+      <Dialog
+        isShown={this.state.isShow}
+        title="Dialog title"
+        onCloseComplete={() => this.setState({ isShown: false })}
+        confirmLabel="Custom Label"
+      >
+        Rename your bucket
+        <TextInput
+          width={300}
+          name="text-input-bucket-name"
+          placeholder="Name"
+          onChange={e => {
+            this._update("bucket", e.target.value);
+          }}
+        />
+      </Dialog>
+    );
+  }
+
+  _renderTable() {
+    const { buckets, bucket } = this.state;
+    return (
+      <>
+        <Dialog
+          isShown={this.state.isShown}
+          title="Rename your bucket"
+          onCloseComplete={() => this.setState({ isShown: false })}
+          confirmLabel="Confirm"
+          onConfirm={() => {
+            this._updateBucket(bucket);
+          }}
+        >
+          <TextInput
+            width={300}
+            name="text-input-bucket-name"
+            placeholder="Name"
+            onChange={e => {
+              this._update("bucket", e.target.value);
+            }}
+          />
+        </Dialog>
+        <Table>
+          <Table.Head>
+            <Table.TextHeaderCell>Bucket name</Table.TextHeaderCell>
+          </Table.Head>
+          <Table.Body height={240}>
+            {buckets.map(bucket => {
+              const { name, id } = bucket;
+              return (
+                <Table.Row key={id}>
+                  <Table.TextCell>{id}</Table.TextCell>
+                  <Table.TextCell>{name}</Table.TextCell>
+                  <Table.TextCell>
+                    <Button
+                      onClick={() => {
+                        this.setState({ isShown: true, id });
+                      }}
+                    >
+                      Edit
+                    </Button>
+                  </Table.TextCell>
+                  <Table.TextCell>Delete</Table.TextCell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+      </>
     );
   }
 
   render() {
+    const { bucket, buckets } = this.state;
     if (this.isLoading) {
       return <h1>Please Wait</h1>;
     } else {
@@ -106,11 +195,11 @@ export default class Dashboard extends Component {
           <Button
             type="primary"
             size="small"
-            onClick={() => this._addBucket(this.state.bucket)}
+            onClick={() => this._addBucket(bucket)}
           >
             Add bucket
           </Button>
-          {this.state.buckets ? this._renderTable() : null}
+          {buckets ? this._renderTable() : null}
         </>
       );
     }
